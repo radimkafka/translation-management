04abad0c909cb36c0dfcd0d4c57c2f02210cea8c
radimkafka <84576481+radimkafka@users.noreply.github.com>
2023-08-24 21:09:40 +0200

    Tests (#3)

--- backend/TranslationManagement.Data/AppDbContext.cs
+++ backend/TranslationManagement.Data/AppDbContext.cs
@@ -5,10 +5,14 @@ namespace TranslationManagement.Data;
 
 public class AppDbContext : DbContext
 {
+    public AppDbContext() : base()
+    {
+    }
+
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
 
-    public DbSet<Job> TranslationJobs { get; set; }
-    public DbSet<Translator> Translators { get; set; }
+    public virtual DbSet<Job> TranslationJobs { get; set; }
+    public virtual DbSet<Translator> Translators { get; set; }
 }
---
+++ backend/TranslationManagement.Test/UpdateJobStatusHandlerTest.cs
@@ -0,0 +1,47 @@
+using Moq;
+using Moq.EntityFrameworkCore;
+using TranslationManagement.Business.Dto;
+using TranslationManagement.Business.Exceptions;
+using TranslationManagement.Business.Handlers;
+using TranslationManagement.Data;
+using TranslationManagement.Data.Entities;
+
+namespace TranslationManagement.Test
+{
+    public class UpdateJobStatusHandlerTest
+    {
+        private readonly UpdateJobStatusHandler _handler;
+
+        private readonly static Job[] FakeData = new[]
+        {
+            new Job { Id = 1, CustomerName = "Customer 1", Status = JobStatusDto.New.ToString() },
+        };
+
+        public UpdateJobStatusHandlerTest()
+        {
+            var appDbContextMock = new Mock<AppDbContext>();
+            appDbContextMock.Setup(x => x.TranslationJobs).ReturnsDbSet(FakeData);
+            _handler = new UpdateJobStatusHandler(appDbContextMock.Object);
+        }
+
+        [Fact]
+        public async Task Status_Update_When_Handle()
+        {
+            var update = new UpdateJobStatusDto() { JobId = 1, Status = JobStatusDto.Inprogress };
+
+            await _handler.Handle(new Business.UpdateJobStatus(update), CancellationToken.None);
+
+            Assert.Equal(FakeData.First().Status, JobStatusDto.Inprogress.ToString());
+        }
+         
+        [Fact]
+        public async Task Status_ThrowNotFoundException_When_Handle()
+        {
+            var update = new UpdateJobStatusDto() { JobId = 2, Status = JobStatusDto.Inprogress };
+
+            async Task act() => await _handler.Handle(new Business.UpdateJobStatus(update), CancellationToken.None);
+
+            await Assert.ThrowsAsync<NotFoundException>(act);
+        }
+    }
+}
