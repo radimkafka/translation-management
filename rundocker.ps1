288de773a8679c6a977934ae431e1beb00fa0097
Radim Kafka <kafkaradim@gmail.com>
2023-08-25 10:41:08 +0200

    run

--- rundocker.ps1
+++ rundocker.ps1
@@ -1,2 +1,5 @@
 docker build -f .\TranslationManagement.Api\Dockerfile --force-rm -t translationmnagementapi .
 docker run -it -p 5000:80 -e ASPNETCORE_URLS=http://+:80 translationmnagementapi 
+
+docker build  -t translationmnagementui .
+docker run -it -p 5137:80 translationmnagementui
